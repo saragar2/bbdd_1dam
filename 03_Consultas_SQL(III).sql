74db3a60b55177f601b826d589585f8d8d6ee99a
Sara García Miguel <146083097+saragar2@users.noreply.github.com>
2024-10-31 12:45:34 +0100

    Add files via upload

--- 03_Consultas_SQL(III).sql
+++ 03_Consultas_SQL(III).sql
@@ -47,6 +47,68 @@ SELECT dept_no, COUNT(1)
     FROM emple
 GROUP BY dept_no;
 -----18-----
+SELECT dept_no, COUNT(1)
+    FROM emple
+GROUP BY dept_no
+HAVING COUNT(1) > 4;
+-----19-----
+SELECT e.dept_no, d.dnombre, COUNT(*) AS total_empleados
+    FROM emple e
+JOIN depart d ON e.dept_no = d.dept_no
+GROUP BY e.dept_no, d.dnombre
+HAVING COUNT(*) > 2;
+-----20-----
+SELECT estante, COUNT(tema)
+    FROM libreria
+GROUP BY estante
+HAVING COUNT(tema) = 3;
+-----21-----
+SELECT SYSDATE
+FROM DUAL;
+-----22-----
+SELECT EXTRACT(month FROM fecha_alt)
+    FROM emple;
+-----23-----
+SELECT ('Hoy es ' || SYSDATE) as hoy
+    FROM dual;
+-----24-----
+SELECT TO_DATE('01012022', 'DDMMYYYY')
+    FROM dual;
+-----25-----
+SELECT 'es el día ' || EXTRACT(day FROM TO_DATE('01012022', 'DDMMYYYY'))
+    FROM dual;
+-----26-----
+SELECT e.apellido, e.oficio, e.emp_no, d.dnombre, d.loc
+    FROM emple e, depart d
+WHERE e.dept_no = d.dept_no;
+-----27-----
+SELECT e.*, d.loc
+    FROM emple e, depart d
+WHERE e.dept_no = d.dept_no
+    AND LOWER(d.loc) IN ('madrid', 'barcelona');
+-----28-----
+SELECT e.apellido, e.oficio, d.loc
+    FROM emple e, depart d
+WHERE e.dept_no = d.dept_no
+    AND LOWER(e.oficio) = 'analista';
+-----29-----
+SELECT d.dnombre, SUM(e.salario), MAX(e.salario), MIN(e.salario)
+    FROM emple e, depart d
+WHERE e.dept_no = d.dept_no
+GROUP BY d.dnombre;
+-----30-----
+SELECT d.dnombre, COUNT(1)
+    FROM emple e, depart d
+WHERE e.dept_no = d.dept_no
+GROUP BY d.dnombre
+HAVING COUNT(1) > 4;
+-----31-----
+
+
+
+
+
+
 -------------------ejemplos
 SELECT COUNT(*), COUNT(dept_no), COUNT(1)
     FROM emple;
@@ -57,4 +119,4 @@ SELECT ROUND(AVG(salario), 2), MAX(salario), MIN(salario)
 SELECT dept_no, oficio, ROUND(AVG(salario), 2)
     FROM emple
 GROUP BY dept_no, oficio;
---- DE 17 A 20, DE 21 A 25 SON FECHAS, 30 Y 30
+--- DE 17 A 20, DE 21 A 25 SON FECHAS, 30 Y 31

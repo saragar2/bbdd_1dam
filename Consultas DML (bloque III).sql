831e02cb15300c74246122ea4b88a2fdabf5f9d6
Sara García Miguel <146083097+saragar2@users.noreply.github.com>
2024-12-03 11:06:25 +0100

    de mal en peor

--- Consultas DML (bloque III).sql
+++ Consultas DML (bloque III).sql
@@ -1,3 +1,7 @@
+--ROLLBACK--
+
+ROLLBACK;
+
 -----1-----
 INSERT INTO profesores VALUES (45, 123, 'Quiroga Martín', 'INFORMÁTICA');
 -----2-----
@@ -47,5 +51,58 @@ UPDATE emple
     SET salario = salario / 2,
         comision = 0
 WHERE dept_no IN (SELECT dept_no
-                        FROM emple) ------sin terminar
+                        FROM emple); ------sin terminar
 -----13-----
+UPDATE emple
+    SET salario = 2 *(SELECT salario
+                        FROM emple
+                    WHERE emp_no = 7369),
+        apellido = LOWER(apellido)
+WHERE dept_no IN (SELECT dept_no
+                        FROM depart
+                    WHERE LOWER(dnombre) = 'contabilidad');
+-----14-----
+DELETE FROM centros
+    WHERE cod_centro = 50; -------------da error porque una fk le hace referencia
+-----15-----
+DELETE FROM centros; -------------------esta tambien
+-----16-----
+
+-----17-----
+DELETE FROM depart
+    WHERE (SELECT COUNT(emp_no)
+                    FROM emple
+                GROUP BY dept_no) < 4; -----está mal
+-----18-----
+INSERT INTO coches VALUES ('TOYOTA');
+INSERT INTO coches VALUES ('BMW');
+-----19-----
+UPDATE coches
+    SET nombre = 'SEAT IBIZA'
+WHERE LOWER(nombre) = 'seat';
+-----20-----
+INSERT INTO alum
+    SELECT *
+        FROM nuevos; -------------------da error porque en la tabla nuevos hay nombres que ya aparecen
+-----21-----
+DELETE FROM alum
+    WHERE nombre IN (SELECT nombre
+                            FROM antiguos);
+-----22-----
+INSERT INTO emple
+    SELECT 200, 'SAAVEDRA', oficio, dir, SYSDATE, salario + (0.20 * salario), comision, dept_no
+        FROM emple
+    WHERE emp_no = 7521;
+-----23-----
+UPDATE emple
+    SET dept_no = (SELECT dept_no
+                        FROM emple
+                    WHERE (SELECT COUNT(emp_no)
+                                FROM emple
+                            GROUP BY dept_no) = (SELECT MAX(COUNT(emp_no))
+                                                    FROM emple
+                                                 GROUP BY dept_no))
+WHERE LOWER(apellido) = 'saavedra'; ----está mal
+-----24-----
+
+-----25-----
